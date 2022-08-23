20194365a71a17e01f9af8916faeda1fc9b5a6d4
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-08-23 23:43:17 +0530

    [Vinaya]Add: Present

---
+++ EmployeeAttendence/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeAttendence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UC1-EmpAttendence");
+            Employee emp = new Employee();
+            emp.EmpWage();
+        }
+    }
+}
