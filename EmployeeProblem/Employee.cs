57686e07f5fcdeed481530db9fb638b0c553f94b
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-08-23 23:33:18 +0530

    Vinaya

---
+++ EmployeeProblem/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeProblem
+{
+    internal class Employee
+    {
+        public void EmpWage()
+        {
+
+        }
+    }
+}
---
+++ EmployeeProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Employee emp = new Employee();
+            emp.EmpWage();
+        }
+    }
+}
