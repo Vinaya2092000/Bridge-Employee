d1ecd1a21c57cd9cb60fedfc254a9175d40bbb01
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-08-24 00:17:39 +0530

    [Vinaya]Add: Wage5

---
+++ EmployeeAttendence/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAttendence
+{
+    internal class Employee
+    {
+        public int EmpPresent = 1;
+        public void EmpWage()
+        {
+            Random empCheck = new Random(); //Random=class random=obj
+            int value = empCheck.Next(0, 2); //next=method
+
+            if (value == EmpPresent)
+            {
+                Console.WriteLine("Employee Present");
+            }
+
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
--- EmployeeProblem/Employee.cs
+++ EmployeeProblem/Employee.cs
@@ -8,45 +8,36 @@ namespace EmployeeProblem
 {
     public class Employee
     {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int RATE_PER_HOUR = 29;
-
-        public const int WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
-
+        public int EmpPresent = 1;
+        public int FullTime = 1;
+        public int Wage_Per_Hr = 20;
+        public int Full_Day_Hr = 8;
+        public int PartTime_Day_Hr = 4;
+        public int DailyWage = 0;
         public void EmpWage()
         {
+            Random empCheck = new Random(); //Random=class random=obj
+            int value = empCheck.Next(0, 2); //next=method
 
-            //Variables
-            int empHrs;
-            int totalEmpHrs = 0, totalWorkingDays = 0;
-            //Computation
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < WORKING_DAYS)
+            if (value == EmpPresent)
             {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
+                Random TimeCheck = new Random();
+                int WorkingHours = TimeCheck.Next(0, 2);
+                if (WorkingHours == FullTime)
                 {
-
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-
-                    default:
-                        empHrs = 6;
-                        break;
+                    DailyWage = Wage_Per_Hr * Full_Day_Hr;
+                    Console.WriteLine("Employee Present" + DailyWage);
                 }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day" + totalWorkingDays + "Emp Hrs " + empHrs);
+                else
+                {
+                    DailyWage = Wage_Per_Hr * PartTime_Day_Hr;
+                    Console.WriteLine("Employee Present for PartTime" + DailyWage);
+                }
             }
-            int totalEmpWage = totalEmpHrs * RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
-        }                                        // DayNumber++;                                                 // TotalWorkingHrs += EmpWorkingHrs;
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }       // TotalWorkingHrs += EmpWorkingHrs;
     }
 }
