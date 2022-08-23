40b3373c98b1a25571d7cbe899f351e693784803
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-08-24 00:25:50 +0530

    [Vinaya]Add: Wage1

--- EmployeeProblem/Employee.cs
+++ EmployeeProblem/Employee.cs
@@ -8,37 +8,21 @@ namespace EmployeeProblem
 {
     public class Employee
     {
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;
-        public const int IS_ABSENT = 0;
-        public const int DailyWage = 0;
-        int empWage = 20;
-        public int IsEmployeePresent()
-        {
-            return new Random().Next(0, 3);
-        }
-
-        //Randon random= new Randon); 
-        //int value = ranoda.next(0,3)
+        public int EmpPresent = 1;
         public void EmpWage()
         {
-            // int DayNumber = 1;
-            int empWorkingHrs = 0;
-            //int TotalworkingHrs = 0;
-            switch (IsEmployeePresent())
+            Random empCheck = new Random(); //Random=class random=obj
+            int value = empCheck.Next(0, 2); //next=method
+
+            if (value == EmpPresent)
             {
-                case IS_ABSENT:
-                    empWorkingHrs = 0;
-                    break;
-                case IS_PART_TIME:
-                    empWorkingHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empWorkingHrs = 8;
-                    break;
+                Console.WriteLine("Employee Present");
             }
-            int DailyWage = empWorkingHrs * empWage; //TotalMage + EmpDailywage;
-            Console.WriteLine("Daily Wage: " + DailyWage);
+
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
